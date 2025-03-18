816d77b5725a0fef0a0b4cdc416679d4ca959e3a
cryvax <aaladan453@gmail.com>
2025-03-18 14:30:12 +0100

    define colors

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color get primary => const Color(0xFF000000);
+  static Color get primary => const Color(0xFF1F3A8B);
+
+  static Color get darkBG => const Color(0xFF111727);
+  static Color get darkCard => const Color(0xFF202938);
+
+  static Color get lightBG => const Color(0xFFF0F0F0);
+  static Color get lightCard => const Color(0xFFF0F0F0);
+
+  static Color get border => const Color(0xFF333C4B);
 }

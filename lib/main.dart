2ba6d40e06b49d8ec1f881e23d92c8d0e32be70d
cryvax <aaladan453@gmail.com>
2025-03-18 14:38:51 +0100

    setup bloc_observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:project_wee/app.dart';
 
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    if (kDebugMode) {
+      debugPrint(
+        '${bloc.runtimeType} ${change.toString()} ${DateTime.now().toString()}',
+      );
+    }
+  }
+}
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Further initializations could go here... Hive...
+
+  Bloc.observer = SimpleBlocObserver();
   runApp(const WeeApp());
 }

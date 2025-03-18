82a159fce597359a9450bd16f685f699ad0ccd46
cryvax <aaladan453@gmail.com>
2025-03-18 14:40:13 +0100

    configure app

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_wee/features/auth/cubit/rider_cubit.dart';
+
+import 'config/route/config.dart';
 
 class WeeApp extends StatelessWidget {
   const WeeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthCubit()),
+      ],
+      child: MaterialApp.router(
+        title: 'Gasopay',
+        routeInformationParser: router.routeInformationParser,
+        routerDelegate: router.routerDelegate,
+        routeInformationProvider: router.routeInformationProvider,
+        backButtonDispatcher: router.backButtonDispatcher,
+      ),
+    );
   }
 }
